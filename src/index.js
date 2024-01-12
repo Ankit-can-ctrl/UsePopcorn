84acc92fe7f9e0263fc04bf9345e8a28d6364b9a
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-01-12 22:44:28 +0530

    Creating a separate Reusable Star component file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import StarRating from "./StarRating";
+// import "./index.css";
+// import App from "./App";
+function Test() {
+  const [moviesRating, setMoviesRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color="blue" onSetRating={setMoviesRating} />
+      <p>This movie was rated {moviesRating} stars.</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating
+      maxRating={5}
+      messages={["Terible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating size={24} color="red" className="test" />
+    <Test />
   </React.StrictMode>
 );
