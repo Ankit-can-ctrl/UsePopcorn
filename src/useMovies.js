deb050796d4bdee9f6d0eb7c86613ed35e3c33c6
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-03-04 23:00:28 +0530

    http to https

--- src/useMovies.js
+++ src/useMovies.js
@@ -15,7 +15,7 @@ export function useMovies(query) {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${Key}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${Key}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok) throw new Error("Something went wrong!");
